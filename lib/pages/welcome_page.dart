01870c7258a9f3c6fbea26027e39ef8748d9c193
Chung Nguyen <zrmedia9@gmail.com>
2021-12-04 21:09:34 +0700

    Project init

---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cubit/misc/colors.dart';
+import 'package:flutter_cubit/widgets/app_text.dart';
+import 'package:flutter_cubit/widgets/responsive_button.dart';
+
+import '../widgets/app_large_text.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  List images = [
+    'welcome-one.png',
+    'welcome-two.png',
+    'welcome-three.png',
+  ];
+
+  List texts = [
+    'Welcome to the app',
+    'This is a sample app',
+    'This is a sample app',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+        itemBuilder: (_, index) {
+          return Container(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('img/' + images[index]),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Container(
+              margin: EdgeInsets.only(top: 150, left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      AppLargeText(text: "Trips"),
+                      AppText(text: "Mountains", size: 30),
+                      SizedBox(height: 20),
+                      Container(
+                        width: 250,
+                        child: AppText(
+                          text:
+                              "Mountain hikes give you an incredidble sense of freedom along with endurane test.",
+                          color: AppColors.textColor2,
+                          size: 14,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      ResponsiveButton(
+                        width: 120,
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: List.generate(3, (indexDots) {
+                      return Container(
+                        margin: EdgeInsets.only(bottom: 2),
+                        width: 8,
+                        height: index == indexDots ? 25 : 8,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: index == indexDots
+                              ? AppColors.mainColor
+                              : AppColors.mainColor.withOpacity(0.3),
+                        ),
+                      );
+                    }),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+        scrollDirection: Axis.vertical,
+        itemCount: images.length,
+      ),
+    );
+  }
+}
